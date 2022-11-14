e231ad7db789f34237009ab3b272466bb65d7d3f
Alisue <lambdalisue@gmail.com>
2022-11-15 01:12:24 +0900

    Upgrade dependencies

--- csi_test.ts
+++ csi_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.164.0/testing/asserts.ts";
 import { Csi, parseCsi } from "./csi.ts";
 
 Deno.test("parseCsi", async (t) => {
